d5195367b78e78ba541dc3621dc11b1e63170fbc
Akash Dholakiya <akashdholakiya222@gmail.com>
2024-08-19 02:27:15 +0530

    Remove .env file from repository

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,30 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getDatabase } from "firebase/database";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhhuKS_JYQ4xI347-Ku9NNyECyN8U2HXQ",
-  authDomain: "schedular-bcf76.firebaseapp.com",
-  databaseURL: "https://schedular-bcf76-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "schedular-bcf76",
-  storageBucket: "schedular-bcf76.appspot.com",
-  messagingSenderId: "958974351798",
-  appId: "1:958974351798:web:85d8cf488bd6902a5b8917",
-  measurementId: "G-SJSG450933"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
-export const rdb = getDatabase(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
